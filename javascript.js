bbf60c754576554462d4915183d3b1d5f5f28aee
Yatrikoxi <tomas_novak@centrum.cz>
2023-03-26 17:53:13 +0200

    comments added

--- javascript.js
+++ javascript.js
@@ -8,12 +8,13 @@ function getComputerChoice() {
     } else (computerChoice === 3)
         return computerResult = "scissors"
 }
-
+// variables that stores player choices and computers choices + points //
 let playerSelection = prompt("Rock, Paper or Scissors???");
 playerSelection = playerSelection.toLowerCase();
 const computerSelection = getComputerChoice();
 let computerPoints = 0;
 let playerPoints = 0;
+// 1 Round function 
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -26,5 +27,6 @@ function playRound(playerSelection, computerSelection) {
     return `"You lose. Computers : "${computerSelection}" beats your: "${playerSelection}"!"`;
     }
 }
+
 console.log(playRound(playerSelection, computerSelection))
 console.log("Player: " + playerPoints + " | " + "Computer: " + computerPoints)
